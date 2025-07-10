0cd94a3a5cfcabf43f981febcc90d2cefce8e50e
unknown <minaev70ivan009@gmail.com>
2025-07-10 15:00:11 +0300

    first commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Погружение - Квесты Рязань",
+	description:
+		"Квесты в Рязани. Погружение - это уникальная организация, специализирующаяся на проведении захватывающих квестов-перформансов.",
+};
+
+export default function Home() {
+	return <div>home</div>;
+}
---
+++ src/components/header/headerLinks.tsx
@@ -0,0 +1,29 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FC } from "react";
+interface Props {
+	links: { text: string; href: string }[];
+}
+
+const HeaderLinks: FC<Props> = ({ links }) => {
+	const path = usePathname();
+	console.log(path);
+	return (
+		<>
+			{links.map((link) => {
+				return (
+					<Link
+						key={link.href}
+						href={link.href}
+						className={`link ${path === link.href && "active"}`}
+					>
+						{link.text}
+					</Link>
+				);
+			})}
+		</>
+	);
+};
+
+export default HeaderLinks;
