ded48cc5f5ab125a390ed0c1b08f440cb3da8bee
Ivan Minaev <minaev70ivan009@gmail.com>
2025-07-16 14:01:18 +0300

    change nextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
 	images: {
-		domains: ["test.outouch.ru", "localhost"],
+		domains: [
+			"test.outouch.ru",
+			"localhost",
+			"quests-ryazan-o11e.vercel.app",
+			"quests-ryazan-o11e-hz9z0czfw-ivans-projects-8be7f86d.vercel.app",
+		],
 	},
 };
 
