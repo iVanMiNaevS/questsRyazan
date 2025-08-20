ed5b07af71531bc2db95f94584d37148b1b9b49c
unknown <minaev70ivan009@gmail.com>
2025-08-20 17:46:55 +0300

    добавил файл индексации

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,13 @@
-import {Metadata} from "next";
+import { Metadata } from "next";
 import styles from "./home.module.scss";
 import Link from "next/link";
 import SwiperUi from "@/components/swiper/swiperUi";
-import {QuestCard} from "@/components/questCard/questCard";
-import {questsService} from "@/services/questsService";
-import {AlertUi} from "@/components/alertUi/alertUi";
-import {reviewService} from "@/services/reviewService";
-import {ReviewsSection} from "@/components/reviewsSection/reviewsSection";
-import {AppRoutes} from "@/utils/linksToPages";
+import { QuestCard } from "@/components/questCard/questCard";
+import { questsService } from "@/services/questsService";
+import { AlertUi } from "@/components/alertUi/alertUi";
+import { reviewService } from "@/services/reviewService";
+import { ReviewsSection } from "@/components/reviewsSection/reviewsSection";
+import { AppRoutes } from "@/utils/linksToPages";
 
 export const metadata: Metadata = {
 	title: "Погружение - Квесты Рязань",
@@ -25,11 +25,20 @@ export default async function Home() {
 		`${process.env.NEXT_PUBLIC_APP_URL}/about_6.314ff98e.webp`,
 		`${process.env.NEXT_PUBLIC_APP_URL}/about_7.ddfeea40.webp`,
 	];
-	const {data: quests, ok: okQuests, message: messageQuests} = await questsService.getQuests();
-	const {data: reviews, ok: okReview, message: messageReview} = await reviewService.getReviews(6);
+	const {
+		data: quests,
+		ok: okQuests,
+		message: messageQuests,
+	} = await questsService.getQuests();
+	const {
+		data: reviews,
+		ok: okReview,
+		message: messageReview,
+	} = await reviewService.getReviews(6);
 	return (
 		<>
-			{!okQuests || (!okReview && <AlertUi message={messageQuests || messageReview} />)}
+			{!okQuests ||
+				(!okReview && <AlertUi message={messageQuests || messageReview} />)}
 
 			<section className={styles.hero}>
 				<div className={styles.hero__video}>
@@ -38,13 +47,17 @@ export default async function Home() {
 					</video>
 					<div className={styles.hero__bg}></div>
 				</div>
-				<div className={`container`} style={{position: "relative", zIndex: "2"}}>
+				<div
+					className={`container`}
+					style={{ position: "relative", zIndex: "2" }}
+				>
 					<div className={styles.hero__textWrap}>
 						<h1 className="h1">Погружение. Квесты в Рязани</h1>
 						<p className="textRegular">
-							Погружение - это уникальная организация, специализирующаяся на проведении
-							захватывающих квестов-перформансов. Наша задача - погрузить участников в увлекательные
-							и эмоциональные приключения, полные загадок, тайн и невероятных испытаний.
+							Погружение - это уникальная организация, специализирующаяся на
+							проведении захватывающих квестов-перформансов. Наша задача -
+							погрузить участников в увлекательные и эмоциональные приключения,
+							полные загадок, тайн и невероятных испытаний.
 						</p>
 					</div>
 					<div className={styles.hero__btns}>
@@ -62,7 +75,9 @@ export default async function Home() {
 					<h2 className="h2">Квесты</h2>
 					<div className={styles.quests__questsWrapp}>
 						{quests
-							? quests.quests.map((quest) => <QuestCard key={quest.id} info={quest} />)
+							? quests.quests.map((quest) => (
+									<QuestCard key={quest.id} info={quest} />
+							  ))
 							: "Квесты не найдены"}
 					</div>
 				</div>
@@ -71,15 +86,20 @@ export default async function Home() {
 				<div className="container">
 					<h2 className="h2">О нас</h2>
 					<p className="textRegular">
-						Мы стремимся создать неповторимые сценарии, где каждая деталь тщательно продумана.
-						Каждый квест - это настоящая история, в которой участники становятся главными героями.
-						Мы создаем особую атмосферу, в которой присутствует ощущение реальности и адреналина.
+						Мы стремимся создать неповторимые сценарии, где каждая деталь
+						тщательно продумана. Каждый квест - это настоящая история, в которой
+						участники становятся главными героями. Мы создаем особую атмосферу,
+						в которой присутствует ощущение реальности и адреналина.
 					</p>
 					<SwiperUi images={images} />
 				</div>
 			</section>
 
-			{reviews ? <ReviewsSection initialReviews={reviews.reviews} /> : "Отзывов пока что нет"}
+			{reviews ? (
+				<ReviewsSection initialReviews={reviews.reviews} />
+			) : (
+				"Отзывов пока что нет"
+			)}
 		</>
 	);
 }
